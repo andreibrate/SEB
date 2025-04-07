7cc5ada39dfdd8bb4a203c415ab9863c06a3ab76
Andrei Brate <andrei.brate@gmail.com>
2025-04-07 14:08:36 +0200

    Added user class as test

--- SEB/obj/Debug/net8.0/SEB.AssemblyInfo.cs
+++ SEB/obj/Debug/net8.0/SEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07623f31d21d39cc8afdac15510858960814d9e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
