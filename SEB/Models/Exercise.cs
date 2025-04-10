a67d78ae4e3ad658afc2dff98ba86045dd70166b
Andrei Brate <andrei.brate@gmail.com>
2025-04-10 11:21:54 +0200

    Added Exercise model (library and history)

---
+++ SEB/Models/Exercise.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEB.Models
+{
+    public class Exercise
+    {
+        public int Count { get; set; }
+        public int Duration { get; set; } // seconds
+        public DateTime Timestamp { get; set; }
+
+        public Exercise()
+        {
+            Count = 0;
+            Duration = 0;
+            Timestamp = DateTime.UtcNow;
+        }
+    }
+}
