4812d814b63d26293d5f4904ad29df63da08f853
Andrei Brate <andrei.brate@gmail.com>
2025-04-21 17:02:07 +0200

    Added HttpResponse

--- SEB/HTTP/HttpResponse.cs
+++ SEB/HTTP/HttpResponse.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SEB.HTTP
 {
-    internal class HttpResponse
+    public class HttpResponse
     {
+        private StreamWriter writer;
+
+        public string HttpVersion { get; set; } = "HTTP/1.0";
+        public int ResponseCode { get; set; } = 200;
+        public string ResponseMessage { get; set; } = "OK";
+        public Dictionary<string, string> Headers { get; set; } = new();
+        public string Body { get; set; }
+
+        public HttpResponse(StreamWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        public void SendResponse()
+        {
+            var writerAlsoToConsole = new StreamTracer(writer);
+
+            writerAlsoToConsole.WriteLine($"{HttpVersion} {ResponseCode} {ResponseMessage}");
+            if (Body != null)
+            {
+                Headers["Content-Length"] = Body.Length.ToString();
+            }
+            foreach (var header in Headers)
+            {
+                writerAlsoToConsole.WriteLine($"{header.Key}: {header.Value}");
+            }
+            writerAlsoToConsole.WriteLine(); // empty line indicates the end of the HTTP-headers
+            if (Body != null)
+            {
+                writerAlsoToConsole.WriteLine(Body);
+            }
+        }
     }
 }
