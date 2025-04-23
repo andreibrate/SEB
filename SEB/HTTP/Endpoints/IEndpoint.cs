3ffaa1166145ed7304ad21e070ebd2e3f725d64b
Andrei Brate <andrei.brate@gmail.com>
2025-04-23 11:45:17 +0200

    Added endpoints interface to handle requests

---
+++ SEB/HTTP/Endpoints/IEndpoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEB.HTTP.Endpoints
+{
+    internal interface IEndpoint
+    {
+        bool HandleRequest(HttpRequest request, HttpResponse response);
+    }
+}
