8d30d2dbd0e11708ecafae1da63498e04ec40235
Andrei Brate <andrei.brate@gmail.com>
2025-04-24 13:27:21 +0200

    Added interfaces for repos (tournament, user, exercise)

---
+++ SEB/DataAccess/Interfaces/ITournamentRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SEB.Models;
+
+namespace SEB.DataAccess.Interfaces
+{
+    public interface ITournamentRepo
+    {
+        void CreateTournament(Tournament tournament);
+        Tournament? GetTournamentById(Guid tournamentId);
+        List<Tournament> GetAllTournaments();
+        void SetWinner(Guid tournamentId, Guid winnerId, bool isDraw);
+    }
+}
---
+++ SEB/DataAccess/Interfaces/IUserRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SEB.Models;
+
+namespace SEB.DataAccess.Interfaces
+{
+    public interface IUserRepo
+    {
+        void RegisterUser(User user);
+        User? LoginUser(string username, string password);
+        User? GetUserById(Guid userId);
+        User? GetUserByUsername(string username);
+        User? GetUserByToken(string token);
+        void UpdateUser(User user);
+    }
+}
