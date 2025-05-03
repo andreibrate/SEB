b9a936f12269a64f045cbee469af6d9312a4d963
Andrei Brate <andrei.brate@gmail.com>
2025-05-03 16:07:34 +0200

    Updated user handler to not catch errors (they are treated in the EP)

--- SEB/BusinessLogic/UserHandler.cs
+++ SEB/BusinessLogic/UserHandler.cs
@@ -20,51 +20,30 @@ namespace SEB.BusinessLogic
         public void Register(User user)
         {
             var existingUser = _userRepo.GetUserByUsername(user.Username);
-            if (existingUser != null)
-            {
-                throw new Exception("Username already taken.");
-            }
-
             _userRepo.RegisterUser(user);
         }
 
-        public User Login(string username, string password)
+        public User? Login(string username, string password)
         {
             var user = _userRepo.LoginUser(username, password);
-            if (user == null)
-            {
-                throw new Exception("Invalid username or password.");
-            }
             return user;
         }
 
-        public User GetUserById(Guid id)
+        public User? GetUserById(Guid id)
         {
             var user = _userRepo.GetUserById(id);
-            if (user == null)
-            {
-                throw new Exception($"User with ID {id} not found.");
-            }
             return user;
         }
 
-        public User GetUserByUsername(string username)
+        public User? GetUserByUsername(string username)
         {
             var user = _userRepo.GetUserByUsername(username);
-            if (user == null)
-            {
-                throw new Exception($"User with username {username} not found.");
-            }
             return user;
         }
 
-        public User GetUserByToken(string token)
+        public User? GetUserByToken(string token)
         {
             var user = _userRepo.GetUserByToken(token);
-            if (user == null)
-            {
-                throw new Exception("Invalid token.");
-            }
             return user;
         }
 
